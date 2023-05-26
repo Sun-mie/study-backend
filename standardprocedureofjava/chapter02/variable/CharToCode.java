0d71342fdf46ecb08b0e1e0826d18a374ec2142f
Sun-mie <ssunm2@kakao.com>
2023-05-26 22:54:23 +0900

    study Standard procedure of Java - chapter02.Variable

---
+++ standardprocedureofjava/chapter02/variable/CharToCode.java
@@ -0,0 +1,13 @@
+package standardprocedureofjava.chapter02.variable;
+
+public class CharToCode {
+    public static void main(String[] args) {
+        char ch = 'A'; // char ch = 65;
+        int code = (int)ch; // ch에 저장된 값을 int타입으로 변환하여 저장한다.
+
+        System.out.printf("%c=%d(%#X)%n", ch, code, code);
+
+        char hch = '기'; // char hch = 0xAC00;
+        System.out.printf("%c=%d(%#X)%n", hch, (int)hch, (int)hch);
+    }
+}
---
+++ standardprocedureofjava/chapter02/variable/SpecialCharEx.java
@@ -0,0 +1,11 @@
+package standardprocedureofjava.chapter02.variable;
+
+public class SpecialCharEx {
+    public static void main(String[] args) {
+        System.out.println('\''); //'
+        System.out.println("abc\t123\b456"); // abc	12456
+        System.out.println('\n'); //엔터
+        System.out.println("\"Hello\""); //"Hello"
+        System.out.println("C:\\"); //C:\
+    }
+}
