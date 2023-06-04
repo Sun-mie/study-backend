d8f050445ea46dcc15614dbdb43763c175fb8e1e
Sun-mie <ssunm2@kakao.com>
2023-06-04 21:16:13 +0900

    study Standard procedure of Java - chapter03.Operator

---
+++ standardprocedureofjava/chapter03/operator/OperatorEx20.java
@@ -0,0 +1,9 @@
+package standardprocedureofjava.chapter03.operator;
+
+public class OperatorEx20 {
+    public static void main(String[] args) {
+        System.out.println(-10%8); // -2
+        System.out.println(10%8); // 2
+        System.out.println(-10%-8); // -2
+    }
+}
