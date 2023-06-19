d91b172fe592cf2bac474cd86e81928e823d5b7f
Sun-mie <ssunm2@kakao.com>
2023-06-19 23:20:35 +0900

    study Standard procedure of Java - chapter05. Array  > multiplication of rows and columns

---
+++ standardprocedureofjava/chapter05/array/ArrayEx23.java
@@ -0,0 +1,11 @@
+package standardprocedureofjava.chapter05.array;
+
+public class ArrayEx23 {
+    public static void main(String[] args) {
+        int[][] m1 = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+        System.out.println(m1.length);
+    }
+}
