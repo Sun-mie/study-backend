f7e5dcce14940392fc5029f4224ab5179ec803b4
Sun-mie <ssunm2@kakao.com>
2023-06-19 21:37:46 +0900

    study Standard procedure of Java - chapter05. Array  > Utilization of multidimensional arrays

---
+++ standardprocedureofjava/chapter05/array/ArrayEx21.java
@@ -0,0 +1,13 @@
+package standardprocedureofjava.chapter05.array;
+
+public class ArrayEx21 {
+    public static void main(String[] args) {
+        char[][] chArr = {
+                {'A', 'B', 'C', 'D', 'E'},
+                {'F', 'G', 'H', 'I'}
+        };
+        for(int i=0; i<chArr.length; i++){
+            System.out.println(chArr[i]);
+        }
+    }
+}
