ded2f10dfb2c2e2a959a4f3beba8694a636d7867
Sun-mie <ssunm2@kakao.com>
2023-06-19 23:28:04 +0900

    study Standard procedure of Java - chapter05. Array  > Guessing the words

--- standardprocedureofjava/chapter05/array/ArrayEx23.java
+++ standardprocedureofjava/chapter05/array/ArrayEx23.java
@@ -1,11 +1,29 @@
 package standardprocedureofjava.chapter05.array;
 
+import java.util.Scanner;
+
 public class ArrayEx23 {
     public static void main(String[] args) {
-        int[][] m1 = {
-                {1, 2, 3},
-                {4, 5, 6}
+        String[][] words = {
+                {"row", "행"},
+                {"column", "열"},
+                {"multiplication", "곱셈"},
+                {"utilization", "활용"}
         };
-        System.out.println(m1.length);
+
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i=0; i<words.length; i++){
+            System.out.printf("Q%d. %s의 뜻은? >", i+1, words[i][0]);
+
+            String tmp = scanner.nextLine();
+
+            if(tmp.equals(words[i][1])){
+                System.out.printf("정답입니다.%n%n");
+            }else{
+                System.out.printf("틀렸습니다. 정답은 %s입니다.%n%n", words[i][1]);
+            }
+        }
+
     }
 }
