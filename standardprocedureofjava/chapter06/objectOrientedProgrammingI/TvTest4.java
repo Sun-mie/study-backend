103e525d4e953b56002fcfac1bb2c2eba86b7592
Sun-mie <ssunm2@kakao.com>
2023-07-02 22:02:52 +0900

    study Standard procedure of Java - chapter06. object-oriented Programming I  > Variable and method

--- standardprocedureofjava/chapter06/objectOrientedProgrammingI/TvTest4.java
+++ standardprocedureofjava/chapter06/objectOrientedProgrammingI/TvTest4.java
@@ -1,30 +1,29 @@
 package standardprocedureofjava.chapter06.objectOrientedProgrammingI;
 
-class Tv3 {
-    //Tv의 속성 (멤버변수)
-    String color;
-    boolean power;
-    int channel;
+class TvTest4 {
+    public static void main(String[] args) {
+        Tv4[] tvArr = new Tv4[3]; // 길이가 3인 Tv객체 배열
 
-    //Tv의 기능 (메서드)
+        // Tv객체를 생성해서 Tv객체 배열의 각 요소에 저장
+        for(int i=0; i<tvArr.length; i++){
+            tvArr[i] = new Tv4();
+            tvArr[i].channel = i+10; // tvArr[i]의 channel에 i+10을 저장
+        }
+
+        for(int i=0; i<tvArr.length; i++){
+            tvArr[i].channelUp(); // tvArr[i]의 메서드를 호출. 채널이 1증가
+            System.out.printf("tvArr[%d].channel = %d%n", i, tvArr[i].channel);
+        }
+
+    }
+}
+
+class Tv4 {
+    String color; //색상
+    boolean power; // 전원상태(on/off)
+    int channel; // 채널
+
     void power() { power = !power; }
     void channelUp() { ++channel; }
     void channelDown() { --channel; }
-}
-
-class TvTest3 {
-    public static void main(String args[]) {;
-        Tv3 t1 = new Tv3();
-        Tv3 t2 = new Tv3();
-        System.out.println("t1의 channel값은 " + t1.channel + "입니다.");
-        System.out.println("t2의 channel값은 " + t2.channel + "입니다.");
-
-        t2 = t1; //t1이 저장하고 있는 값(주소)을 t2에 저장
-
-        t1.channel = 7;
-        System.out.println("t1의 channel값을 7로 변경하였습니다.");
-
-        System.out.println("t1의 channel값은 " + t1.channel + "입니다.");
-        System.out.println("t2의 channel값은 " + t2.channel + "입니다.");
-    }
 }
