1a09340e03a13641ad51907609224dffbc41691d
Sun-mie <ssunm2@kakao.com>
2023-07-07 00:08:53 +0900

    study Standard procedure of Java - chapter06. object-oriented Programming I  > Variable and method

---
+++ standardprocedureofjava/chapter06/objectOrientedProgrammingI/MainTest.java
@@ -0,0 +1,7 @@
+package standardprocedureofjava.chapter06.objectOrientedProgrammingI;
+
+public class MainTest {
+    public static void main(String[] args) {
+        main(null); //재귀호출 자기 자신을 다시 호출
+    }
+}
