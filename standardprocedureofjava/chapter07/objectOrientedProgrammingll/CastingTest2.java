fd760cc429f3eb972caf571a291baacf4160953c
Sun-mie <ssunm2@kakao.com>
2023-08-02 00:25:16 +0900

    study Standard procedure of Java - chapter07. object-oriented Programming II  > Up-casting, Down-casting

---
+++ standardprocedureofjava/chapter07/objectOrientedProgrammingll/CastingTest2.java
@@ -0,0 +1,15 @@
+package standardprocedureofjava.chapter07.objectOrientedProgrammingll;
+
+public class CastingTest2 {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Car car2 = null;
+        FireEngine fe = null;
+
+        car.drive();
+        fe = (FireEngine) car;
+        fe.drive();
+        car2 = fe;
+        car2.drive();
+    }
+}
